00891b3cf9481a018e6373994a0564e8f6ca5bd7
davidotieno <davidotieno473@gmail.com>
2023-09-28 06:54:28 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,17 @@
+function introduction(name){
+name="otieno"
+console.log(`Hi, my name is ${name}.`);
+}
+introduction();
+
+function logTwoValues(value1, value2) {
+    value1=3
+    value2=4
+    console.log(`The two values are ${value1} and ${value2}.`);
+  }
+  logTwoValues();
+  function introductionWithLanguageOptional(name, language="javascript") {
+    name="otieno"
+    console.log(`my name is ${name} and i am learning ${language}.`);
+  }
+  introductionWithLanguageOptional();
